8f8092be1bcc6edb5e9eeee93d4228ca757e4f7c
englisa2-oss <englisa2@tcnj.edu>
2025-12-11 20:13:29 -0500

    Enhance comments for data analysis insights
    Added notes to descriptive statistics and chi-squared test results.

--- Englisa-final_project.R
+++ Englisa-final_project.R
@@ -11,12 +11,13 @@ data <- read_csv("data.csv")
 # descriptive statistics
 table(data$political_party)
 table(data$previous_experience)
-
+# Note: Reveals that, that 2 political parties (republican and democrat) are split with 221 Republicans / 220 are Democrats. for a total of 441. 369 have prior experience / 72 have no prior experience.
 
 # contingency table
 table(data$political_party, data$previous_experience)
+#NOTE: 164 Republicans have Prior experience / 57 do not. 205 Democrats have prior experience. 15 do not.
+#NOTE: 26% of Republicans have no Prior experience. 0.07% of Democrats have no Prior experience.
+#NOTE: A total of 17% of Representatives have no prior experience.# chi squared test
 
-# chi squared test
 chisq.test(table(data$political_party, data$previous_experience))
-           
-    
+#NOTE: There is a statistical and practical difference. This is seen with a p-value of 1.431e-07.     
